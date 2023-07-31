55974ade0f0a8b20c4005385733548fa66bd6442
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-07-31 14:52:40 +0900

    ServletConfig 클래스에서 상속받은 WebMvcConfigurer의 오버라이드 메서드가 나타나지 않는 오류 확인

---
+++ src/main/java/org/zerock/config/ServletConfig.java
@@ -0,0 +1,11 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.zerock.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+	
+}
