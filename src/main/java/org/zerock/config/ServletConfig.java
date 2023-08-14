a997a5bb222ee1acdfa8e2a0a456d231685c89c8
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-14 17:02:44 +0900

    ex01과 동일한 pom.xml 설정 적용, 파일 업로드 처리 자바 설정 적용, controller에서 확인하는 방법 필요

--- src/main/java/org/zerock/config/ServletConfig.java
+++ src/main/java/org/zerock/config/ServletConfig.java
@@ -1,6 +1,11 @@
 package org.zerock.config;
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -25,6 +30,27 @@ public class ServletConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
+	//파일 업로드 처리
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getResolver() throws IOException{
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		
+		// 10MB
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+		
+		// 2MB
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+		
+		// 1MB
+		resolver.setMaxInMemorySize(1024 * 1024);
+		
+		// temp upload
+		resolver.setUploadTempDir(new FileSystemResource("C:\\upload\\tmp"));
+		resolver.setDefaultEncoding("UTF-8");
+		
+		return resolver;
 	}
 	
 }
