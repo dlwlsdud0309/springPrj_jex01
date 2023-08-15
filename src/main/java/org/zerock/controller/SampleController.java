16544d9a0ae87f962dff08be0bfc5d2175e13618
dlwlsdud9439 <dlwlsdud9439@gmail.com>
2023-08-15 20:05:14 +0900

    @RunWith 어노테이션의 import 오류, 확인 필요

---
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,8 @@
+package org.zerock.controller;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SampleController {
+
+}
